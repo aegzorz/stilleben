31bcd201f52fe0b546b31320f49d13d6dd6ebbf9
Alexander Persson <aegzorz@gmail.com>
2022-05-20 23:19:59 +0200

    ✨ Adding DiffingStrategy protocol

---
+++ Sources/Stilleben/Diffing/DiffingStrategy.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public protocol DiffingStrategy {
+    associatedtype Value
+
+    func diff(actual: Value, expected: Value) async throws -> Diff<Value>
+}
+
+public enum Diff<Value> {
+    case same
+    case different(artifacts: [Artifact])
+
+    public struct Artifact {
+        public let value: Value
+        public let description: String
+    }
+}
