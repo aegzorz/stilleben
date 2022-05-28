8f996d138b90437a6944bbeafcdacff9b0177bc8
Alexander Persson <aegzorz@gmail.com>
2022-05-28 23:40:06 +0200

    ✅ Added some test cases

--- Tests/StillebenTests/StillebenTests.swift
+++ Tests/StillebenTests/StillebenTests.swift
@@ -1,11 +1,28 @@
 import XCTest
+import SwiftUI
 @testable import Stilleben
 
 final class StillebenTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Stilleben().text, "Hello, World!")
+    func testSwiftUISnapshot() async throws {
+        try await Snapshot {
+            ScrollView {
+                Text("Hello World")
+                    .border(Color.red)
+            }
+        }
+        .diffSwiftUI()
+        .match()
+    }
+
+    func testJsonSnapshot() async throws {
+        struct Test: Encodable {
+            var title = "Hello World!"
+        }
+
+        try await Snapshot {
+            Test()
+        }
+        .diffJson()
+        .match()
     }
 }
