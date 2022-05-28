b870f020bc0423cb843199a91c768942be15a9e3
Alexander Persson <aegzorz@gmail.com>
2022-05-28 17:28:40 +0200

    ✨ Added Snapshot struct

---
+++ Sources/Stilleben/Snapshot/Snapshot.swift
@@ -0,0 +1,27 @@
+import Foundation
+import XCTest
+
+public struct Snapshot<Value> {
+    public typealias Produce = () async throws -> Value
+
+    public let produce: Produce
+    public let context: SnapshotContext
+
+    public init(file: StaticString = #file, function: StaticString = #function, line: UInt = #line, produce: @escaping Produce) {
+        self.produce = produce
+        context = SnapshotContext(
+            callsite: Callsite(file: file, function: function, line: line)
+        )
+    }
+
+    internal init(context: SnapshotContext, produce: @escaping Produce) {
+        self.context = context
+        self.produce = produce
+    }
+
+    public func map<T>(_ transform: @MainActor @escaping (Value) async throws -> T) -> Snapshot<T> {
+        Snapshot<T>(context: context) {
+            try await transform(produce())
+        }
+    }
+}
