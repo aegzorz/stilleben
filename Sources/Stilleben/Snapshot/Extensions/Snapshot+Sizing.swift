4fca09dfcd6cebebe966e1e0ab41e0ee74c9d823
Alexander Persson <aegzorz@gmail.com>
2022-05-29 13:05:57 +0200

    ✨ Extending Snapshot to use sizing strategy, size is stored in context

---
+++ Sources/Stilleben/Snapshot/Extensions/Snapshot+Sizing.swift
@@ -0,0 +1,23 @@
+import Foundation
+import UIKit
+import SwiftUI
+
+public extension SnapshotContext.Key where Value == CGSize {
+    static var targetSize: Self {
+        SnapshotContext.Key(name: "targetSize")
+    }
+}
+
+extension Snapshot where Value: UIViewController {
+    public func size(using strategy: SizingStrategy?) -> Snapshot<UIViewController> {
+        map { viewController in
+            if let strategy = strategy {
+                context.set(
+                    value: strategy.size(viewController: viewController),
+                    for: .targetSize
+                )
+            }
+            return viewController
+        }
+    }
+}
