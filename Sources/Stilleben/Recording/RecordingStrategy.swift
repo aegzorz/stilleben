a77f69932be12824d9fb1f3385c5d0a013b879d6
Alexander Persson <aegzorz@gmail.com>
2022-05-29 09:03:15 +0200

    ✨ Added isRecordingSnapshots to force recording

--- Sources/Stilleben/Recording/RecordingStrategy.swift
+++ Sources/Stilleben/Recording/RecordingStrategy.swift
@@ -1,6 +1,8 @@
 import Foundation
 import UIKit
 
+public var isRecordingSnapshots = false
+
 public protocol RecordingStrategy {
     func read(context: SnapshotContext) async throws -> Data?
     func write(data: Data, context: SnapshotContext) async throws
--- Sources/Stilleben/Snapshot/Extensions/Snapshot+Data.swift
+++ Sources/Stilleben/Snapshot/Extensions/Snapshot+Data.swift
@@ -5,14 +5,21 @@ public extension Snapshot where Value == Data {
     struct MissingReferenceError: Error {
         let localizedDescription = "No recording found"
     }
+    struct RecordingModeEnabled: Error {
+        let localizedDescription = "Recording snapshots mode enabled"
+    }
 
     func record(using strategy: RecordingStrategy) -> Snapshot<(Data, Data)> {
         map { actual in
+            if isRecordingSnapshots {
+                try await strategy.write(data: actual, context: context)
+                throw RecordingModeEnabled()
+            }
+
             if let expected = try await strategy.read(context: context) {
                 return (actual, expected)
             } else {
                 try await strategy.write(data: actual, context: context)
-                XCTFail("No recording found")
                 throw MissingReferenceError()
             }
         }
