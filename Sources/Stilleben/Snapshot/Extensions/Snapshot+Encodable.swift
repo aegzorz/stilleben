3c12b00c4e3497059735adeb4f368c7c3acccf60
Alexander Persson <aegzorz@gmail.com>
2022-05-29 13:00:48 +0200

    ♻️ Setting file extension in context

--- Sources/Stilleben/Snapshot/Extensions/Snapshot+Encodable.swift
+++ Sources/Stilleben/Snapshot/Extensions/Snapshot+Encodable.swift
@@ -2,20 +2,16 @@ import Foundation
 
 extension Snapshot where Value: Encodable {
     func diffJson() -> Snapshot<Diff> {
-        encodeJson().record(using: .json).diff(using: .text)
+        encodeJson().record(using: .localFile).diff(using: .text)
     }
 
     public func encodeJson() -> Snapshot<Data> {
-        map { value in
+        context.set(value: "json", for: .fileExtension)
+
+        return map { value in
             let encoder = JSONEncoder()
             encoder.outputFormatting = [.sortedKeys, .prettyPrinted]
             return try encoder.encode(value)
         }
-    }
-}
-
-extension RecordingStrategy where Self == LocalFileRecordingStrategy {
-    public static var json: LocalFileRecordingStrategy {
-        localFile(fileExtension: "json")
     }
 }
