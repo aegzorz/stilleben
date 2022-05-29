345dacfe0d0cc5b3e53f1e0e964d797188dba059
Alexander Persson <aegzorz@gmail.com>
2022-05-29 13:00:23 +0200

    ♻️ Setting file extension in context

--- Sources/Stilleben/Snapshot/Extensions/Snapshot+UIImage.swift
+++ Sources/Stilleben/Snapshot/Extensions/Snapshot+UIImage.swift
@@ -3,18 +3,14 @@ import UIKit
 
 public extension Snapshot where Value == UIImage {
     func record(using strategy: RecordingStrategy) -> Snapshot<(UIImage, UIImage)> {
-        map { image in
+        context.set(value: "png", for: .fileExtension)
+
+        return map { image in
             try image.pngData().unwrap()
         }
         .record(using: strategy)
         .map { (actual, expected) in
             (try UIImage(data: actual).unwrap(), try UIImage(data: expected).unwrap())
         }
-    }
-}
-
-extension RecordingStrategy where Self == LocalFileRecordingStrategy {
-    public static var png: LocalFileRecordingStrategy {
-        localFile(fileExtension: "png")
     }
 }
