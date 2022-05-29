01d647c9a5752ac9685317cef202820a0c69e754
Alexander Persson <aegzorz@gmail.com>
2022-05-29 13:28:01 +0200

    ♻️ No optionals

--- Sources/Stilleben/Snapshot/Extensions/Snapshot+Sizing.swift
+++ Sources/Stilleben/Snapshot/Extensions/Snapshot+Sizing.swift
@@ -9,14 +9,12 @@ public extension SnapshotContext.Key where Value == CGSize {
 }
 
 extension Snapshot where Value: UIViewController {
-    public func size(using strategy: SizingStrategy?) -> Snapshot<UIViewController> {
+    public func size(using strategy: SizingStrategy) -> Snapshot<UIViewController> {
         map { viewController in
-            if let strategy = strategy {
-                context.set(
-                    value: try await strategy.size(viewController: viewController),
-                    for: .targetSize
-                )
-            }
+            context.set(
+                value: try await strategy.size(viewController: viewController),
+                for: .targetSize
+            )
             return viewController
         }
     }
