d7a4d7412438d851dc9f1a96ae24c694c181b612
Alexander Persson <aegzorz@gmail.com>
2022-05-29 12:59:56 +0200

    ♻️ Refactored SnapshotContext to be a class, added configuration property

--- Sources/Stilleben/Snapshot/SnapshotContext.swift
+++ Sources/Stilleben/Snapshot/SnapshotContext.swift
@@ -1,5 +1,28 @@
 import Foundation
 
-public struct SnapshotContext {
+public final class SnapshotContext {
     public let callsite: Callsite
+    internal var configuration: [String: Any] = [:]
+
+    public init(callsite: Callsite) {
+        self.callsite = callsite
+    }
+}
+
+extension SnapshotContext {
+    public struct Key<Value>: Hashable {
+        public let name: String
+
+        public init(name: String) {
+            self.name = name
+        }
+    }
+
+    public func set<T>(value: T, for key: Key<T>) {
+        configuration[key.name] = value
+    }
+
+    public func value<T>(for key: Key<T>) -> T? {
+        configuration[key.name] as? T
+    }
 }
