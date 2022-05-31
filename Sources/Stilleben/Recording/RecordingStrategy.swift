19ca601c05e8382d3bd7c9d41df6d4f311f3c442
Alexander Persson <aegzorz@gmail.com>
2022-06-01 00:32:26 +0200

    🔥 Removed global var

--- Sources/Stilleben/Recording/RecordingStrategy.swift
+++ Sources/Stilleben/Recording/RecordingStrategy.swift
@@ -1,8 +1,6 @@
 import Foundation
 import UIKit
 
-public var isRecordingSnapshots = false
-
 public protocol RecordingStrategy {
     func read(context: SnapshotContext) async throws -> Data?
     func write(data: Data, context: SnapshotContext) async throws
