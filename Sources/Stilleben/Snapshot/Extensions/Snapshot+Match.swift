9029702769c1c0125d881d26af06ec4dc445d774
Alexander Persson <aegzorz@gmail.com>
2022-06-01 00:30:13 +0200

    ♻️ Catching errors and converting them into XCTFail

--- Sources/Stilleben/Snapshot/Extensions/Snapshot+Match.swift
+++ Sources/Stilleben/Snapshot/Extensions/Snapshot+Match.swift
@@ -2,19 +2,23 @@ import Foundation
 import XCTest
 
 extension Snapshot where Value == Diff {
-    public func match(file: StaticString = #file, line: UInt = #line) async throws {
-        try await map { diff in
-            switch diff {
-            case .same:
-                return
-            case .different(let description, let attachments):
-                XCTContext.runActivity(named: context.callsite.functionName) { activity in
-                    attachments.forEach(activity.add)
-                }
+    public func match(file: StaticString = #file, line: UInt = #line) async {
+        do {
+            try await map { diff in
+                switch diff {
+                case .same:
+                    return
+                case .different(let description, let attachments):
+                    XCTContext.runActivity(named: context.callsite.functionName) { activity in
+                        attachments.forEach(activity.add)
+                    }
 
-                return XCTFail(description, file: file, line: line)
+                    return XCTFail(description, file: file, line: line)
+                }
             }
+            .produce()
+        } catch {
+            XCTFail(error.localizedDescription, file: file, line: line)
         }
-        .produce()
     }
 }
