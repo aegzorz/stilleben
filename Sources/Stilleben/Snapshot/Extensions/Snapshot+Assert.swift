850d09fe20ce645bbb61766f753c950a68cce036
Alexander Persson <aegzorz@gmail.com>
2022-06-09 09:14:29 +0200

    ♻️ Updated assertions

--- Sources/Stilleben/Snapshot/Extensions/Snapshot+Assert.swift
+++ Sources/Stilleben/Snapshot/Extensions/Snapshot+Assert.swift
@@ -4,13 +4,13 @@ import XCTest
 public extension Snapshot {
     func assertSimulator(modelIdentifier expected: String, file: StaticString = #filePath, line: UInt = #line) -> Self {
         let actual = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "Unknown"
-        XCTAssert(actual == expected, "Wrong simulator used, expected: \(expected) actual: \(actual)", file: file, line: line)
+        XCTAssertEqual(actual, expected, "Wrong simulator used", file: file, line: line)
         return self
     }
 
     func assertDisplayScale(_ expected: CGFloat, file: StaticString = #filePath, line: UInt = #line) -> Self {
         let actual = UIScreen.main.scale
-        XCTAssert(actual == expected, "Wrong display scale detected, expected: \(expected) actual: \(actual)", file: file, line: line)
+        XCTAssertEqual(actual, expected, "Wrong display scale detected", file: file, line: line)
         return self
     }
 }
