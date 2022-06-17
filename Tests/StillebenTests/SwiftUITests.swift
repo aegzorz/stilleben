0af5ba9671f59d2555774c49282dac49b0954ec8
Alexander Persson <aegzorz@gmail.com>
2022-06-18 00:25:54 +0200

    ♻️ Extracting item list to separate file

--- Tests/StillebenTests/SwiftUITests.swift
+++ Tests/StillebenTests/SwiftUITests.swift
@@ -72,20 +72,3 @@ final class SwiftUITests: XCTestCase {
             }
     }
 }
-
-private struct ItemList: View {
-    let count: Int
-
-    var body: some View {
-        ScrollView {
-            ForEach(0..<count, id: \.self) { index in
-                Text("Item #\(index + 1)")
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(colors[index % colors.count].opacity(0.6))
-            }
-        }
-    }
-
-    private let colors: [Color] = [.red, .blue, .green, .yellow, .purple]
-}
