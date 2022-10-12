0c9249c2cd899b77d1fdca62f879af98937ee977
Alexander Persson <aegzorz@gmail.com>
2022-10-12 19:31:11 +0200

    ♻️ Optional params for assert functions

--- Sources/Stilleben/Snapshot/Snapshot/Snapshot+Assert.swift
+++ Sources/Stilleben/Snapshot/Snapshot/Snapshot+Assert.swift
@@ -4,7 +4,9 @@ import XCTest
 public extension Snapshot {
     /// Assert that a specific iOS simulator is being used
     /// - Parameter modelIdentifier: The expected model used to take snapshots, i.e. "iPhone10,1" for iPhone 8
-    func assertSimulator(modelIdentifier expected: String, file: StaticString = #filePath, line: UInt = #line) -> Self {
+    func assertSimulator(modelIdentifier expected: String?, file: StaticString = #filePath, line: UInt = #line) -> Self {
+        guard let expected else { return self }
+
         let actual = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "Unknown"
         XCTAssertEqual(actual, expected, "Wrong simulator used", file: file, line: line)
         return self
@@ -12,7 +14,9 @@ public extension Snapshot {
 
     /// Assert that a specific display scale is being used.
     /// - Parameter expected: The expected display scale (1.0, 2.0 or 3.0 for example)
-    func assertDisplayScale(_ expected: CGFloat, file: StaticString = #filePath, line: UInt = #line) -> Self {
+    func assertDisplayScale(_ expected: CGFloat?, file: StaticString = #filePath, line: UInt = #line) -> Self {
+        guard let expected else { return self }
+
         let actual = UIScreen.main.scale
         XCTAssertEqual(actual, expected, "Wrong display scale detected", file: file, line: line)
         return self
