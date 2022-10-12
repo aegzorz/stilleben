c09c91e155f4c2bf4a088b1619e9b898c1925621
Alexander Persson <aegzorz@gmail.com>
2022-10-12 22:17:39 +0200

    ♻️ Passing in encoder

--- Sources/Stilleben/Snapshot/Snapshot/Snapshot+Encodable.swift
+++ Sources/Stilleben/Snapshot/Snapshot/Snapshot+Encodable.swift
@@ -1,23 +1,16 @@
 import Foundation
+import Combine
 
 extension Snapshot where Value: Encodable {
     /// Allows diffing any `Encodable` as JSON, recording using `localFile` recording strategy
-    func diffJson() -> Snapshot<Diff> {
+    public func diffJson() -> Snapshot<Diff> {
         encodeJson().record(using: .localFile).diff(using: .text)
     }
 
-    public typealias Configure = (JSONEncoder) -> Void
-
-    public func encodeJson(_ configure: Configure? = nil) -> Snapshot<Data> {
+    public func encodeJson(encoder: JSONEncoder = JSONEncoder()) -> Snapshot<Data> {
         context.set(value: "json", for: .fileExtension)
 
         return map { value in
-            let encoder = JSONEncoder()
-            if let configure = configure {
-                configure(encoder)
-            } else {
-                encoder.outputFormatting = [.sortedKeys, .prettyPrinted]
-            }
             return try encoder.encode(value)
         }
     }
