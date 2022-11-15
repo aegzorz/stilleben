727d088bd550c8dad298e7b94443c4bc1410acb2
Alexander Persson <aegzorz@gmail.com>
2022-11-15 22:32:15 +0100

    ♻️ Compensating for safe area insets

--- Sources/Stilleben/Sizing/IntrinsicSizeStrategy.swift
+++ Sources/Stilleben/Sizing/IntrinsicSizeStrategy.swift
@@ -9,7 +9,12 @@ public extension SizingStrategy where Self == IntrinsicSizeStrategy {
 /// Sizing strategy that sizes the snapshots according to the intrinsic size of the supplied view controller
 public struct IntrinsicSizeStrategy: SizingStrategy {
     public func size(viewController: UIViewController, context: SnapshotContext) -> CGSize {
-        let size = viewController.view.intrinsicContentSize
+        let insets = viewController.view.safeAreaInsets
+        viewController.additionalSafeAreaInsets = UIEdgeInsets(top: -insets.top, left: 0, bottom: -insets.bottom, right: 0)
+
+        var size = viewController.view.intrinsicContentSize
+        size.height -= insets.top + insets.bottom
+
         // If size is not useful, try system sizing
         if size.height <= 0 || size.width <= 0 {
             return viewController.view.systemLayoutSizeFitting(.zero)
